43fe30513d053e847244523e8374e1ec60c016ef
Syed Arsalan Khan <khansyed231am@gmail.com>
2023-03-01 21:28:38 +0500

    complete

---
+++ reddit-clone/src/chakra/theme.tsx
@@ -0,0 +1,17 @@
+export const theme = {
+   colors: {
+      brand: {
+         100: '#FF3c00'
+      },
+   },
+   fonts: {
+      body: "Open Sans, sans-serif",
+   },
+   styles: {
+      global: () => ({
+         body: {
+            bg: "gray.200",
+         },
+      }),
+   },
+}

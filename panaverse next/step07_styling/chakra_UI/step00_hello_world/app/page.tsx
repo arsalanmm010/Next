bb59ab2f91086cc58ac46d8036fbc6d3d018031b
Syed Arsalan Khan <khansyed231am@gmail.com>
2023-03-13 23:00:24 +0500

    Chakra UI hello world

---
+++ panaverse next/step07_styling/chakra_UI/step00_hello_world/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { Button } from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    
+    <div><Button colorScheme='blue' m='50px'>Click Me</Button></div>
+  )
+}
